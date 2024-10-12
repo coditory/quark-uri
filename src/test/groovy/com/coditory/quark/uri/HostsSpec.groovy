7d0ebe57227e299f16ee4f94fb7321e6f99c279b
ogesaku <ogesaku@gmail.com>
2024-10-12 23:29:44 +0200

    Update publish commands 4

--- src/test/groovy/com/coditory/quark/uri/HostsSpec.groovy
+++ src/test/groovy/com/coditory/quark/uri/HostsSpec.groovy
@@ -3,17 +3,17 @@ package com.coditory.quark.uri
 import spock.lang.Specification
 
 class HostsSpec extends Specification {
-    def "should retrieve non blank localHostAddress"() {
-        when:
-            String localHostAddress = Hosts.localHostAddress
-        then:
-            !localHostAddress.isBlank()
-    }
-
     def "should retrieve non blank localHostName"() {
         when:
             String localHostName = Hosts.localHostName
         then:
             !localHostName.isBlank()
+    }
+
+    def "should retrieve non blank localHostAddress"() {
+        when:
+            String localHostAddress = Hosts.localHostAddress
+        then:
+            !localHostAddress.isBlank()
     }
 }
